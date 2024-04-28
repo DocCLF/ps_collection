c3fd740a62b2b73b4d68a940c3795e4cb52d246e
ToolDoc <mailtodoc@web.de>
2024-04-28 16:03:49 +0200

    add some stuff for later use

--- IBM_Host_Volume_Map.ps1
+++ IBM_Host_Volume_Map.ps1
@@ -14,7 +14,34 @@ function IBM_Host_Volume_Map {
     #>
     [CmdletBinding()]
     param (
-        
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$UserName,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$DeviceIP,
+        [Parameter(ValueFromPipeline)]
+        [string]$FilterName
     )
+    begin{
+        $TD_Mappingresault = @()
+        $TD_CollectVolInfo = ssh $UserName@$DeviceIP "lshostvdiskmap -delim : && lsvdisk -delim : $FilterName"
+        $TD_CollectVolInfo = $TD_CollectVolInfo |Select-Object -Skip 1
+    }
+    process{
+        foreach($line in $TD_CollectVolInfo){
+            if(($line | Select-String -Pattern '\b(\w+)' -AllMatches).Matches.Value[0] -eq "id"){Write-Host "<###### --- Ende --- ######>" -ForegroundColor Red;break}
+            $TD_SplitInfos = "" | Select-Object HostID,HostName,HostCluster,VolumeID,VolumeName,UID
+            $TD_SplitInfos.HostID = ($line | Select-String -Pattern '\b(\w+)' -AllMatches).Matches.Value[0]
+            $TD_SplitInfos.HostName = ($line | Select-String -Pattern '\b(\w+)' -AllMatches).Matches.Value[1]
+            $TD_SplitInfos.HostCluster = ($line | Select-String -Pattern '\b(\w+)' -AllMatches).Matches.Value[10]
+            $TD_SplitInfos.VolumeID = ($line | Select-String -Pattern '\b(\w+)' -AllMatches).Matches.Value[3]
+            $TD_SplitInfos.VolumeName = ($line | Select-String -Pattern '\b(\w+)' -AllMatches).Matches.Value[4]
+            $TD_SplitInfos.UID = ($line | Select-String -Pattern '\b(\w+)' -AllMatches).Matches.Value[5]
+            
+            $TD_Mappingresault += $TD_SplitInfos
+        }
+    }
+    end{
+
+    }
     
 }
