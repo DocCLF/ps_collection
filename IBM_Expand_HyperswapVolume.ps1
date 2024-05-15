107087d6f8a0644d784d4dd444e577c9abc808da
ToolDoc <mailtodoc@web.de>
2024-05-15 09:26:48 +0200

    fix a filter bug

--- IBM_Expand_HyperswapVolume.ps1
+++ IBM_Expand_HyperswapVolume.ps1
@@ -19,7 +19,7 @@ function IBM_Expand_HyperswapVolume {
     .LINK
         https://www.ibm.com/docs/en/flashsystem-5x00/8.3.x?topic=commands-expandvolume
     .EXAMPLE
-        IBM_Expand_HyperswapVolume -UserName superuser -DeviceIP 192.16.12.1 -expand_size 128849018880 -unit b
+        IBM_Expand_HyperswapVolume -UserName superuser -DeviceIP 192.16.12.1 -expand_size 10 -unit gb
 
         Result: svctask expandvolume -size 10 -unit gb ExampleVolume_01    
     .EXAMPLE
@@ -51,25 +51,24 @@ function IBM_Expand_HyperswapVolume {
     $ErrorActionPreference="SilentlyContinue"
     <# count to split into 10-liner #>
     [Int16]$i=0
-    $TD_CollectVolInfo = ssh $UserName@$DeviceIP "lsvdisk"
+    $TD_CollectVolInfo = ssh $UserName@$DeviceIP "lsvdisk -delim :"
     Start-Sleep -Seconds 3
     foreach($TD_info in $TD_CollectVolInfo) {
-        $TD_Vol_Info = ($TD_info | Select-String -Pattern '^\d+\s+([a-zA-Z0-9_-]*)\s+' -AllMatches).Matches.Groups.Value[1]
+        $TD_Vol_Info = ($TD_info | Select-String -Pattern '^\d+:([a-zA-Z0-9_-]*)' -AllMatches).Matches.Groups.Value[1]
         Write-Debug -Message $TD_Vol_Info
-        if($TD_Vol_Info -like "$($FilterName)*"){
+        if($TD_Vol_Info -like "$($FilterName)"){
             <# To prevent duplicate entries #>
             if($TD_Temp -eq $TD_Vol_Info){break}
             <# Returns the command for the cli. #>
             if($expand_size -gt 0) {
                 if($unit -eq ""){Write-Host "If a expand size is specified, we also need a size specification of a unit such as kb,mb,gb,tb, etc.!" -ForegroundColor Red; Start-Sleep -Seconds 5; exit}
                 Write-Host "svctask expandvolume -size $expand_size -unit $unit $TD_Vol_Info"
-                <# split with 2 line of nothing and reset i #>
-                $i++
-                if($i -eq 10){Write-Host "`n `n"; $i = 0}
             }else {
-                
                 Write-host $TD_Vol_Info
             }
+            <# split with 2 line of nothing and reset i #>
+            $i++
+            if($i -eq 10){Write-Host "`n `n"; $i = 0}
         }
         <# Copy the current value to the temp value for the duplicate check #>
         $TD_Temp = $TD_Vol_Info
